b262123b18d06c30f2868bb8bda345542a49a67b
LIN_CHIEH <linchieh@stu.comp.nus.edu.sg>
2023-05-20 20:56:06 +0800

    Implement messages page

--- app/components/Sidebar/DesktopSidebar.tsx
+++ app/components/Sidebar/DesktopSidebar.tsx
@@ -4,9 +4,19 @@ import useRoutes from "@/app/hooks/useRoutes";
 import { useState } from "react";
 import DesktopItem from "./DesktopItem";
 
-const DesktopSidebar = () => {
+import { User } from '@prisma/client';
+import Avatar from "../avatar";
+
+interface DesktopSidebarProps {
+    currentUser: User
+}
+
+const DesktopSidebar: React.FC<DesktopSidebarProps> = ({
+    currentUser
+}) => {
     const routes = useRoutes();
     const [isOpen, setIsOpen] = useState(false);
+    console.log({currentUser});
     return ( 
         <div
         className="
@@ -50,7 +60,24 @@ const DesktopSidebar = () => {
                         />
                     ))}
                 </ul>
-
+            </nav>
+            <nav className="
+                mt-4
+                flex
+                flex-col
+                justify-between
+                items-center
+            ">
+                <div 
+                    onClick={() => setIsOpen(true)}
+                    className="
+                        cursor-pointer
+                        hover:opacity-75
+                        transition
+                    "
+                >
+                    <Avatar user={currentUser}/>
+                </div>
             </nav>
         </div> 
     );
